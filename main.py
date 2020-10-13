e93d32d6429594a5568d5471d2dc067a977b660e
Appuchia <46938448+appuchias@users.noreply.github.com>
2020-10-13 23:00:04 +0200

    One liner

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
+from os import path
 
 with open("paths.txt") as r:
     source = r.readlines()[0]
     dest = r.readlines()[1]
 
 for root, subfolders, files in os.walk(source):
-    root_list = root.split("\\")
-    folder = root_list[-1:][0]
+    folder = path.split(root)[1] # "split("c:/dir")" returns "("c:/", "dir")" so I take the directory
     for file in files:
         os.rename(f'{root}/{file}', f'{dest}/{folder}-{file}')
     print(folder)
