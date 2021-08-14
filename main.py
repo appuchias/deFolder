82069d1564958073d12292469bd6eb388a8ba225
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-08-14 21:29:56 +0200

    Last version

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import os
 from os import path
+import sys
 
-with open("paths.txt") as r:
-    source = r.readlines()[0]
-    dest = r.readlines()[1]
+try:
+    with open("paths.txt") as r:
+        source = r.readlines()[0]
+        dest = r.readlines()[1]
+except:
+    try:
+        source = sys.argv[1]
+        dest = sys.argv[2]
+    except:
+        print("Provide paths either though the file or the CL")
 
 for root, subfolders, files in os.walk(source):
-    folder = path.split(root)[1] # "split("c:/dir")" returns "("c:/", "dir")" so I take the directory
+    folder = path.split(root)[
+        1
+    ]  # 'split("c:/dir")' returns '("c:/", "dir")' so I take the directory
     for file in files:
-        os.rename(f'{root}/{file}', f'{dest}/{folder}-{file}')
+        os.rename(f"{root}/{file}", f"{dest}/{folder}-{file}")
     print(folder)
 
 print("Program finished!")
