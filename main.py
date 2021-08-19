d30c00bcc7ccc74699014a597a42e94000b5c0a2
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-08-19 23:56:35 +0200

    Add comments, spacing and implement verbose option

--- main.py
+++ main.py
@@ -6,38 +6,22 @@ from pathlib import Path
 # Checks if there is a folder inside the specified path
 def is_there_a_folder(src_path: str) -> bool:
     assert path.exists(src_path), "Provided path does not exist."
+
+    # If there is a folder, break the loop and return True
     for element in os.scandir(src_path):
         if element.is_dir():
             break
+
     else:
         return False
+
     return True
 
 
-# Removes folders recurrsively moving files upwards in the directory tree. Returns false if no folders were removed
-def recursive_child_remover(src_path: Path) -> bool:
+# Removes folders recurrsively moving files upwards in the directory tree. Returns False if no folders were deleted
+def remode_child_folders(src_path: Path, is_verbose: bool) -> bool:
     assert path.exists(src_path), "Provided path does not exist."
-    parent = src_path.parent
-
-    # while is_there_a_folder(src_path):
-    #     for element in os.scandir(src_path):
-    #         if element.is_dir():
-    #             recursive_child_remover(Path(element.path))
-    #         elif element.is_file():
-    #             dest_path = path.join(parent, element.name)
-
-    #             if args.verbose:
-    #                 print(
-    #                     element.path,
-    #                     "→",
-    #                     path.join(dest_path, str(src_path) + "-" + element.name),
-    #                 )
-
-    #             os.rename(
-    #                 element.path,
-    #                 path.join(dest_path, str(src_path) + "-" + element.name),
-    #             )
-    # os.rmdir(src_path)
+    result = True
 
     # While added to repeat the loop in consecutive folders
     while is_there_a_folder(src_path):
@@ -59,17 +43,23 @@ def recursive_child_remover(src_path: Path) -> bool:
                     )
 
                     # UX
-                    print(
-                        file_path,
-                        "->",
-                        path.join(src_path, str(file_path.parent) + "-" + element),
-                    )
+                    if is_verbose:
+                        print(
+                            file_path,
+                            "->",
+                            path.join(src_path, str(file_path.parent) + "-" + element),
+                        )
 
         # 'If' to avoid removing root folder, remove every other child folder
         if Path(root) != src_path:
+            if result:
+                result = False
             os.rmdir(root)
 
+    return result
 
+
+# Default behaviour
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -81,7 +71,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-v",
         "--verbose",
-        help="Only print changes, do not move files",
+        help="Print all changes made",
         action="store_true",
     )
 
@@ -89,6 +79,8 @@ if __name__ == "__main__":
 
     src_folder = Path(path.abspath(args.src_folder))
 
-    recursive_child_remover(src_folder)
+    were_folders_deleted = remode_child_folders(src_folder, args.verbose)
 
-    print("Done")
+    print(
+        "Done", "Folders were deleted" if were_folders_deleted else ""
+    ) if args.verbose else ""
